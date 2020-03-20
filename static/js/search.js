ca7043681976d940641ad85cb0913d0002db3c5e
Simey de Klerk <simeydeklerk@gmail.com>
2020-03-20 23:03:07 +0200

    add autosearch javascript to results.html

---
+++ static/js/search.js
@@ -0,0 +1,31 @@
+// got _ from https://unpkg.com/lodash
+
+const form = document.getElementById('search-form')
+const searchBox = document.getElementById('search-box')
+const resultsNode = document.getElementById('search-results')
+
+// form.addEventListener('submit', handleChange)
+searchBox.addEventListener('input', _.debounce(handleChange, 100))
+
+function handleChange(e) {
+    e.preventDefault()
+    const term = searchBox.value
+    if (term) {
+        cachedEva(term).then(text => searchBox.value === term? resultsNode.innerHTML = text : '')
+    } else {
+        resultsNode.innerHTML = ''
+    }
+}
+
+function fetchEva(term) {
+    const url = `/search?raw=1&q=${term}`
+    return fetch(url).then(res => res.text())
+}
+
+const cachedEva = cacheFn(fetchEva)
+
+function cacheFn(fn) { // caching (aka memoization) via a closure
+    const cache = {}
+    const cachedFn = (x) => cache[x] = cache[x] || fn(x)
+    return cachedFn
+}
