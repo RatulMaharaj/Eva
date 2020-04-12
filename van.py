f83fd83e634166122afe18e3e6d2d86b0f234bc1
Ratul Maharaj <RMaharaj3@oldmutual.com>
2020-04-12 21:34:54 +0200

    added placeholder to password box input

--- van.py
+++ van.py
@@ -6,8 +6,8 @@ OPTIONS = {
     "use_pipreqs": True,
     'install_only_these_modules':[],
     "exclude_modules":[],
-    "include_modules":['.\\combine.py','.\\search.py','.\\update.py'],
-    "path_to_get_pip_and_python_embeded_zip": ""
+    "include_modules":['.\\combine.py','.\\search.py','.\\update.py','.\\encrypt.py'],
+    "path_to_get_pip_and_python_embeded_zip": R"C:\Users\x457537\Documents\Projects\Assets\Embed",
 }
 
 pyvan.build(OPTIONS)
