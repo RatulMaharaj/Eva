3afc175685863f965344c4a536d7f32b79aaca14
Simey de Klerk <simeydeklerk@gmail.com>
2020-06-15 14:37:45 +0200

    add modified time

--- src/components/Browse/FsItem.js
+++ src/components/Browse/FsItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFolder, faCog } from '@fortawesome/free-solid-svg-icons';
+import { formatDistanceToNow, parse } from 'date-fns'
 import filesize from "filesize"
 
 import getIcon from "./icon.js"
@@ -8,7 +9,7 @@ import getIcon from "./icon.js"
 import './FsItem.css'
 
 function FsItem({ item, setPath = () => { } }) {
-    const { name, is_folder, size_bytes, path, hidden, read_only, system } = item;
+    const { name, is_folder, size_bytes, path, hidden, read_only, system, modified_time } = item;
     const icon = is_folder ? faFolder : getIcon(name);
     let href = "", onClick = () => { }, size = "";
     if (is_folder) {
@@ -19,24 +20,27 @@ function FsItem({ item, setPath = () => { } }) {
         };
     }
     else {
-        size = <span className="size">{filesize(size_bytes, {round: 1})}</span>
+        size = <span className="size">{filesize(size_bytes, { round: 1 })}</span>
     }
+
+const modified = modified_time ? <span className="mod-time">{formatDistanceToNow(new Date(modified_time)) + ' ago'}</span> : ''
 
     const className = [
         'item',
         read_only ? 'read-only' : '',
-        hidden ? 'hidden': '',
-        system ? 'system': '',
+        hidden ? 'hidden' : '',
+        system ? 'system' : '',
     ].join(' ')
 
     return (<li className={className}>
         <OptionalA href={href} className="item-link" onClick={onClick}>
             <div className="item-icon-wrapper">
                 <FontAwesomeIcon icon={icon} fixedWidth className="item-icon" />
-                {system ? <FontAwesomeIcon icon={faCog} className="system-icon" /> : "" }
+                {system ? <FontAwesomeIcon icon={faCog} className="system-icon" /> : ""}
             </div>
             <span className="name">{name}</span>
             {size}
+            {modified}
         </OptionalA>
     </li>);
 }
