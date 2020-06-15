c27d2decf1a51a92e2bb8ee9fff1e25fb2e0945a
Simey de Klerk <simeydeklerk@gmail.com>
2020-06-15 10:37:00 +0200

    add hidden and system styling

--- src/components/Browse/FsItem.js
+++ src/components/Browse/FsItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolder, faFile } from '@fortawesome/free-solid-svg-icons';
+import { faFolder, faCog } from '@fortawesome/free-solid-svg-icons';
 import filesize from "filesize"
 
 import getIcon from "./icon.js"
@@ -8,7 +8,7 @@ import getIcon from "./icon.js"
 import './FsItem.css'
 
 function FsItem({ item, setPath = () => { } }) {
-    const { name, is_folder, size_bytes, path } = item;
+    const { name, is_folder, size_bytes, path, hidden, read_only, system } = item;
     const icon = is_folder ? faFolder : getIcon(name);
     let href = "", onClick = () => { }, size = "";
     if (is_folder) {
@@ -21,10 +21,19 @@ function FsItem({ item, setPath = () => { } }) {
     else {
         size = <span className="size">{filesize(size_bytes, {round: 1})}</span>
     }
-    return (<li className="item">
+
+    const className = [
+        'item',
+        read_only ? 'read-only' : '',
+        hidden ? 'hidden': '',
+        system ? 'system': '',
+    ].join(' ')
+
+    return (<li className={className}>
         <OptionalA href={href} className="item-link" onClick={onClick}>
             <div className="item-icon-wrapper">
                 <FontAwesomeIcon icon={icon} fixedWidth className="item-icon" />
+                {system ? <FontAwesomeIcon icon={faCog} className="system-icon" /> : "" }
             </div>
             <span className="name">{name}</span>
             {size}
