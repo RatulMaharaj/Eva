cdd1240257eb80c54aa5d75ee5e7cd0167aab95e
Simey de Klerk <simeydeklerk@gmail.com>
2020-06-20 20:48:13 +0200

    Fix typo in update

--- api/src/update.py
+++ api/src/update.py
@@ -15,7 +15,7 @@ def write_folders(folders, folders_location):
 def update(database_location, folders_location):
      # These are the folders we crawl through to collect information
     source_folders = read_folders(folders_location)
-    frames = [pd.DataFrame(index_folder(f),) for f in source_folders]
+    frames = [pd.DataFrame(index_folder(f)) for f in source_folders]
     df = pd.concat(frames)
     df.fillna(0)
     df.to_csv(database_location, index = False, na_rep=0)
