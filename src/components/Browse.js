2217b7f1d568b14aabbd575bd313eb947853c7f8
Simey de Klerk <simeydeklerk@gmail.com>
2020-06-20 23:18:02 +0200

    Add caching to fetching Browse items

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useQueryParam, StringParam } from 'use-query-params';
-
+import cachedFetch from "../utils/cachedFetch";
 import Locationbar from "./Browse/Locationbar";
 import FsItem from "./Browse/FsItem";
 
 import "./iconColors.css"
 import "./Browse.css"
-
 
 function sortFiles(a, b) {
     if (a.hidden !== b.hidden) { return a.hidden ? 1 : -1 }
@@ -16,13 +15,13 @@ function sortFiles(a, b) {
 function Roots({setPath}) {
     const [roots, setRoots] = useState([])
     useEffect(() => {
-        fetch('./api/settings')
+        cachedFetch('./api/settings')
         .then(res => res.json())
         .then(x => setRoots(x.folders.split('\n')))
     }, [])
 
     return roots.map(root => {
-        return <FsItem item={{ name: root, path: "", is_folder: true, num_files: 0, num_subfolders: 0, folder_size_bytes: 0 }} setPath={setPath} />
+        return <FsItem key={root} item={{ name: root, path: "", is_folder: true, num_files: 0, num_subfolders: 0, folder_size_bytes: 0 }} setPath={setPath} />
     })
 }
 
@@ -30,9 +29,12 @@ function useItems(path, initialItems = []) {
     const [items, setItems] = useState(initialItems)
     useEffect(() => {
         const url = `/api/browse?path=${path}`
-        fetch(url)
+        cachedFetch(url)
             .then(res => res.json())
-            .then(results => setItems(results.results.sort(sortFiles)))
+            .then(({results}) => {
+                // results.filter(item => item.is_folder).forEach(({path, name}) => cachedFetch(`/api/browse?path=${path ? (path + '\\' + name) : name}`))
+                setItems(results.sort(sortFiles))
+            })
     }, [path])
 
     return items
@@ -42,6 +44,7 @@ function Browse() {
 
     const [path, setPath] = useQueryParam("path", StringParam);
     const items = useItems(path)
+    useEffect(()=> {if(!path) {setPath("")}},[path, setPath])
 
     return (
         <div className="area">
@@ -50,7 +53,7 @@ function Browse() {
             <div className="results-area">
                 <ul className="results-list">
                     {(path === "") ? <Roots setPath={setPath}/> : ''}
-                    {items.map(item => <FsItem item={item} setPath={setPath} />)}
+                    {items.map(item => <FsItem key={item.name} item={item} setPath={setPath} />)}
                 </ul>
                 {/* <pre>{JSON.stringify(items, null, 2)}</pre> */}
             </div>
