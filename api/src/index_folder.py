3f3ccc366c469a10e9792115dc5bef03b8b8d3f2
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-05 17:48:46 +0200

    Fixed formatting

--- api/src/index_folder.py
+++ api/src/index_folder.py
@@ -1,25 +1,27 @@
-import os, stat
+import os
+import stat
 from datetime import datetime
 
-LOCATION = 'path'
-NAME = 'name'
-IS_FOLDER = 'is_folder'
-SIZE = 'size_bytes'
-ATIME = 'accessed_time'
-MTIME = 'modified_time'
-CTIME = 'created_time'
-READONLY = 'readonly'
-HIDDEN = 'hidden'
-SYSTEM = 'system'
+LOCATION = "path"
+NAME = "name"
+IS_FOLDER = "is_folder"
+SIZE = "size_bytes"
+ATIME = "accessed_time"
+MTIME = "modified_time"
+CTIME = "created_time"
+READONLY = "readonly"
+HIDDEN = "hidden"
+SYSTEM = "system"
 
-FOLDER_SIZE = 'folder_size_bytes'
-NUM_FILES = 'num_files'
-NUM_FOLDERS = 'num_subfolders'
+FOLDER_SIZE = "folder_size_bytes"
+NUM_FILES = "num_files"
+NUM_FOLDERS = "num_subfolders"
+
 
 def index_folder(path):
-    '''
-        return a generator of scandir results for all the files and folders in a folder, including total size, filecount and foldercount for each folder
-    '''
+    """
+    return a generator of scandir results for all the files and folders in a folder, including total size, filecount and foldercount for each folder
+    """
 
     if isinstance(path, dict):
         root_dict = path
@@ -27,11 +29,11 @@ def index_folder(path):
         root_dict = direntry_to_dict(direntry_for_one_folder(path))
 
     root_dict[FOLDER_SIZE] = root_dict[NUM_FILES] = root_dict[NUM_FOLDERS] = 0
-    root_str = os.path.join(root_dict[LOCATION],root_dict[NAME])
+    root_str = os.path.join(root_dict[LOCATION], root_dict[NAME])
 
     for dir_entry in os.scandir(root_str):
         entry_dict = direntry_to_dict(dir_entry)
-        if entry_dict[IS_FOLDER]: #If the child is a folder
+        if entry_dict[IS_FOLDER]:  # If the child is a folder
             try:
                 for descendant in index_folder(entry_dict):
                     root_dict[FOLDER_SIZE] += descendant[SIZE]
@@ -39,10 +41,10 @@ def index_folder(path):
                         root_dict[NUM_FOLDERS] += 1
                     else:
                         root_dict[NUM_FILES] += 1
-                    yield descendant 
+                    yield descendant
             except PermissionError:
                 yield entry_dict
-        else: # if the child is a file
+        else:  # if the child is a file
             root_dict[FOLDER_SIZE] += entry_dict[SIZE]
             root_dict[NUM_FILES] += 1
             yield entry_dict
@@ -50,9 +52,9 @@ def index_folder(path):
 
 
 def direntry_to_dict(de):
-    '''
-        convert a direntry object to a simple dict
-    '''
+    """
+    convert a direntry object to a simple dict
+    """
     result = {
         LOCATION: os.path.normpath(os.path.split(de.path)[0]),
         NAME: de.name,
@@ -62,30 +64,30 @@ def direntry_to_dict(de):
     return result
 
 
-def stat_to_dict(stats, results_dict = {}):
-    '''
-        convert a stat object to a simple dict
-        or append it to an existing dict
-    '''
-    
-    results_dict[SIZE]  = stats.st_size
-    results_dict[ATIME] = datetime.fromtimestamp(stats.st_atime).replace(microsecond = 0)
-    results_dict[MTIME] = datetime.fromtimestamp(stats.st_mtime).replace(microsecond = 0)
-    results_dict[CTIME] = datetime.fromtimestamp(stats.st_ctime).replace(microsecond = 0)
-    
-    if os.name == 'nt': # Only on Windows
+def stat_to_dict(stats, results_dict={}):
+    """
+    convert a stat object to a simple dict
+    or append it to an existing dict
+    """
+
+    results_dict[SIZE] = stats.st_size
+    results_dict[ATIME] = datetime.fromtimestamp(stats.st_atime).replace(microsecond=0)
+    results_dict[MTIME] = datetime.fromtimestamp(stats.st_mtime).replace(microsecond=0)
+    results_dict[CTIME] = datetime.fromtimestamp(stats.st_ctime).replace(microsecond=0)
+
+    if os.name == "nt":  # Only on Windows
         attributes = stats.st_file_attributes
         results_dict[READONLY] = (stat.FILE_ATTRIBUTE_READONLY & attributes) != 0
         results_dict[HIDDEN] = (stat.FILE_ATTRIBUTE_HIDDEN & attributes) != 0
         results_dict[SYSTEM] = (stat.FILE_ATTRIBUTE_SYSTEM & attributes) != 0
-    
+
     return results_dict
 
 
 def direntry_for_one_folder(path):
-    '''
-        returns a single DirEntry object for one path
-    '''
+    """
+    returns a single DirEntry object for one path
+    """
     base, name = os.path.split(path)
     for entry in os.scandir(base):
         if entry.name == name:
