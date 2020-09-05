95aa5e9cfed00afa9b173f81ae498d5ec1f7f7da
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-05 17:46:49 +0200

    create DEP_folder cross platform

--- api/api.py
+++ api/api.py
@@ -1,87 +1,111 @@
-import datetime
-from flask import Flask, render_template, request, send_file, redirect, jsonify
+from flask import Flask, request, jsonify
 from src.update import update, read_folders, write_folders
 import src.search as Search
 import pandas as pd
-import json
-import os, glob
-from waitress import serve
+import os
+import platform
+# import json
+# from waitress import serve
 
-version = '0.3.0'
+version = "0.3.0"
 
 # Set app parameters
+if platform.system() == "Darwin":
+    DEP_FOLDER = "../dependencies/"  # macOS
+else:
+    DEP_FOLDER = "..\\dependencies\\"  # Windows
 
-# UPLOAD_FOLDER = ".\\uploads"
-DEP_FOLDER = "..\\dependencies\\"
 DATABASE_LOCATION = DEP_FOLDER + "database.csv"
 FOLDERS_LOCATION = DEP_FOLDER + "folders.txt"
 DEFAULT_SEARCH_RESULT_LIMIT = 100
-IS_UPDATING = 'no'
+IS_UPDATING = "no"
 
 # Create dependencies that don't exist if needed
 
 if not os.path.isdir(DEP_FOLDER):
     os.mkdir(DEP_FOLDER)
-    
 if not os.path.isfile(DATABASE_LOCATION):
-    f = pd.DataFrame(columns = Search.COLUMNS)
+    f = pd.DataFrame(columns=Search.COLUMNS)
     f.to_csv(DATABASE_LOCATION)
-
 if not os.path.isfile(FOLDERS_LOCATION):
     f = open(FOLDERS_LOCATION, "w+")
     f.close()
 
 # Create app instance
-app = Flask(__name__, static_folder="../build",static_url_path="/")
+app = Flask(__name__, static_folder="../build", static_url_path="/")
 
 Search.database_location = DATABASE_LOCATION
 Search.load_data()
 
+
 # Serve built react app
-@app.route('/')
+@app.route("/")
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file("index.html")
+
 
 # api endpoints
-@app.route('/api/search', methods=['GET'])
+
+
+@app.route("/api/search", methods=["GET"])
 def search():
-    query = request.args.get('q')
+    query = request.args.get("q")
     # raw = (request.args.get('raw') or "") != ""
-    limit = int(request.args.get('limit') or DEFAULT_SEARCH_RESULT_LIMIT)
+    limit = int(request.args.get("limit") or DEFAULT_SEARCH_RESULT_LIMIT)
     if query == "":
-        results_dict = [{'name':'Search for a file', 'path':'Use the search box above to find a file on the share drives'}]
-        return jsonify(results=results_dict, searchcriteria=query, hits=0, returned_hits=0)
-    
+        results_dict = [
+            {
+                "name": "Search for a file",
+                "path": "Use the search box above to find a file on the share drives",
+            }
+        ]
+        return jsonify(
+            results=results_dict, searchcriteria=query, hits=0, returned_hits=0
+        )
+
     elif query:
         results = Search.searchcsv(query)
         hits = len(results)
         if hits == 0:
             returned_hits = 0
-            results_dict = [{'name':'No files were found!', 'path':'Please adjust your search criteria and try again.'}]
+            results_dict = [
+                {
+                    "name": "No files were found!",
+                    "path": "Please adjust your search criteria and try again.",
+                }
+            ]
         else:
             if limit > 0 and hits > limit:
                 results = results.head(limit)
             returned_hits = len(results)
-            results_dict = results.to_dict('records')
-        return jsonify(results=results_dict, searchcriteria=query, hits=hits, returned_hits=returned_hits)
+            results_dict = results.to_dict("records")
+        return jsonify(
+            results=results_dict,
+            searchcriteria=query,
+            hits=hits,
+            returned_hits=returned_hits,
+        )
     else:
         return "No valid query parameters used"
 
-@app.route('/api/settings', methods=['GET', 'POST'])
+
+@app.route("/api/settings", methods=["GET", "POST"])
 def update_data():
-    if request.method == 'POST':
-        global IS_UPDATING 
-        IS_UPDATING = 'yes'
+    if request.method == "POST":
+        global IS_UPDATING
+        IS_UPDATING = "yes"
         data = request.get_json()
-        folders = data['folders'].splitlines()
+        folders = data["folders"].splitlines()
         write_folders(folders, FOLDERS_LOCATION)
         update(DATABASE_LOCATION, FOLDERS_LOCATION)
         Search.load_data()
-        IS_UPDATING = 'no'        
+        IS_UPDATING = "no"
         return jsonify(message="update successful")
 
     folders = read_folders(FOLDERS_LOCATION)
-    folders_str = '\n'.join(folders)
+    folders_str = "\n".join(folders)
     modtime = Search.getmodtime()
 
-    return jsonify(modtime = modtime, version = version, folders=folders_str, isUpdating=IS_UPDATING)
+    return jsonify(
+        modtime=modtime, version=version, folders=folders_str, isUpdating=IS_UPDATING
+    )
--- api/app.py
+++ api/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, send_file
 from update import update, read_folders, write_folders
+
 # from encrypt import encrypt_file
 import src.search as Search
+
 # from combine import merge_pdfs
 import pandas as pd
 import json
 import os, glob
 from waitress import serve
 
-version = '0.2.0'
+version = "0.2.0"
 
 UPLOAD_FOLDER = ".\\uploads"
 DEP_FOLDER = "..\\Eva - Dependencies\\"
@@ -18,96 +20,127 @@ FOLDERS_LOCATION = DEP_FOLDER + "folders.txt"
 DEFAULT_SEARCH_RESULT_LIMIT = 100
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 # ui = FlaskUI(app=app,maximized=True)
 
 Search.database_location = DATABASE_LOCATION
 Search.update_data()
 
-@app.route('/')
-def home():
-    return render_template('home.html')
 
-@app.route('/search')
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/search")
 def search():
-    query = request.args.get('q') or ""
-    raw = (request.args.get('raw') or "") != ""
-    limit = int(request.args.get('limit') or DEFAULT_SEARCH_RESULT_LIMIT)
+    query = request.args.get("q") or ""
+    raw = (request.args.get("raw") or "") != ""
+    limit = int(request.args.get("limit") or DEFAULT_SEARCH_RESULT_LIMIT)
     if query:
         results = Search.searchcsv(query)
         hits = len(results)
         if hits == 0:
             returned_hits = 0
-            results_dict = [{'name':'No files were found!', 'path':'Please adjust your search criteria and try again.'}]
+            results_dict = [
+                {
+                    "name": "No files were found!",
+                    "path": "Please adjust your search criteria and try again.",
+                }
+            ]
         else:
             if limit > 0 and hits > limit:
                 results = results.head(limit)
             returned_hits = len(results)
-            results_dict = results.to_dict('records')
-        template = 'results.html' if not raw else 'results_raw.html'
-        return render_template(template, results=results_dict, searchcriteria=query, hits=hits, returned_hits=returned_hits)
+            results_dict = results.to_dict("records")
+        template = "results.html" if not raw else "results_raw.html"
+        return render_template(
+            template,
+            results=results_dict,
+            searchcriteria=query,
+            hits=hits,
+            returned_hits=returned_hits,
+        )
     else:
-        return render_template('search.html')
+        return render_template("search.html")
 
-@app.route('/about')
+
+@app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
-@app.route('/settings', methods=['GET', 'POST'])
+
+@app.route("/settings", methods=["GET", "POST"])
 def update_data():
     folders = read_folders(FOLDERS_LOCATION)
-    
-    if request.method == 'POST':
-        folders = request.form['folders'].splitlines()
+
+    if request.method == "POST":
+        folders = request.form["folders"].splitlines()
         write_folders(folders, FOLDERS_LOCATION)
         update(DATABASE_LOCATION, FOLDERS_LOCATION)
         Search.update_data()
 
-    folders_str = '\n'.join(folders)
+    folders_str = "\n".join(folders)
     modtime = Search.getmodtime()
-    return render_template('update.html',modtime = modtime, version = version, folders=folders_str)
+    return render_template(
+        "update.html", modtime=modtime, version=version, folders=folders_str
+    )
 
 
 def clear_uploads():
-    files = glob.glob(UPLOAD_FOLDER+'\\*')
+    files = glob.glob(UPLOAD_FOLDER + "\\*")
     for file in files:
         os.remove(file)
 
-@app.route('/combine', methods=['GET','POST'])
+
+@app.route("/combine", methods=["GET", "POST"])
 def combine():
-    if request.method == 'POST':
+    if request.method == "POST":
         clear_uploads()
         try:
             files = request.files.getlist("files")
-            password = request.form.get('password')
+            password = request.form.get("password")
             names = []
-            print("The password is:",password)
-            for file in files: # save files to folder
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-                names.append(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-            
+            print("The password is:", password)
+            for file in files:  # save files to folder
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+                names.append(os.path.join(app.config["UPLOAD_FOLDER"], file.filename))
+
             names.sort()
 
-            merge_pdfs(names, output=os.path.join(app.config['UPLOAD_FOLDER'],'merged.pdf'))
+            merge_pdfs(
+                names, output=os.path.join(app.config["UPLOAD_FOLDER"], "merged.pdf")
+            )
 
             if password != "":
-                print('The password is not blank')
-                encrypt_file(os.path.join(app.config['UPLOAD_FOLDER'],'merged.pdf'), password, mode = "encrypt")
-                return send_file(os.path.join(app.config['UPLOAD_FOLDER'],'merged_encrypted.pdf'), as_attachment=True)
-            return send_file(os.path.join(app.config['UPLOAD_FOLDER'],'merged.pdf'), as_attachment=True)
+                print("The password is not blank")
+                encrypt_file(
+                    os.path.join(app.config["UPLOAD_FOLDER"], "merged.pdf"),
+                    password,
+                    mode="encrypt",
+                )
+                return send_file(
+                    os.path.join(app.config["UPLOAD_FOLDER"], "merged_encrypted.pdf"),
+                    as_attachment=True,
+                )
+            return send_file(
+                os.path.join(app.config["UPLOAD_FOLDER"], "merged.pdf"),
+                as_attachment=True,
+            )
         except:
-            return render_template('combine.html')
+            return render_template("combine.html")
     else:
-        return render_template('combine.html')
+        return render_template("combine.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Create dependencies that don't exist if needed
     if not os.path.isdir(DEP_FOLDER):
         os.mkdir(DEP_FOLDER)
-        
+
     if not os.path.isfile(DATABASE_LOCATION):
-        f = pd.DataFrame(columns = Search.COLUMNS)
+        f = pd.DataFrame(columns=Search.COLUMNS)
         f.to_csv(DATABASE_LOCATION)
 
     if not os.path.isfile(FOLDERS_LOCATION):
@@ -116,6 +149,3 @@ if __name__ == '__main__':
 
     app.run(debug=True)
     # serve(app, host='127.0.0.1', port=5050)
-
- 
-    
