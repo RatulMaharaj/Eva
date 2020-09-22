9f001e39e955bb51217d09ea46765bb9501d0e4d
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-22 06:04:57 +0200

    removed array keys

--- src/components/About.js
+++ src/components/About.js
@@ -10,30 +10,8 @@ function About() {
         <br />
 
         <p>
-          Eva is a multi-purpose toolkit whose purpose is to assist it's users
-          with their daily tasks.
-        </p>
-        <br />
-        <br />
-
-        <h3>CURRENT APPLICATIONS</h3>
-        <br />
-        <p> Below is a list of the current applications available on Eva: </p>
-        <br />
-
-        <ul className="applist">
-          <li>Ask Eva - A shared drive search application.</li>
-          <br />
-          <li>
-            Eva PDF - An application that can be used to merge and/or encrypt
-            pdf files.
-          </li>
-          <br />
-        </ul>
-
-        <p>
-          Eva is continuously being tweaked and improved. More features will be
-          added in the future.
+          Eva is a search tool whose purpose is to assist it's users
+          with finding files quickly and easily.
         </p>
         <br />
         <br />
