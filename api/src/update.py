44cce1f89976b494cf9a2d1c4789b107090736b8
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-22 04:55:52 +0200

    write data to sqlite3 db

--- api/src/update.py
+++ api/src/update.py
@@ -1,21 +1,38 @@
 import pandas as pd
+import sqlite3
+import datetime as datetime
 from .index_folder import index_folder
 
+
 def read_folders(folders_location):
-    with open(folders_location, 'r') as f:
+    with open(folders_location, "r") as f:
         text = f.read()
-    folders = text.split('\n')
+    folders = text.split("\n")
     return folders
 
+
 def write_folders(folders, folders_location):
-    text = '\n'.join(folders)
-    with open(folders_location, 'w') as f:
+    text = "\n".join(folders)
+    with open(folders_location, "w") as f:
         f.write(text)
 
+
 def update(database_location, folders_location):
-     # These are the folders we crawl through to collect information
+    # These are the folders we crawl through to collect information
+    start_time = datetime.now()  # start timestamp
+    conn = sqlite3.connect(database_location)  # connect to database
+
     source_folders = read_folders(folders_location)
     frames = [pd.DataFrame(index_folder(f),) for f in source_folders]
+
     df = pd.concat(frames)
     df.fillna(0)
-    df.to_csv(database_location, index = False, na_rep=0)
+    df.to_sql("ask_eva", conn, if_exists="replace")  # write data to database table
+
+    end_time = datetime.now()  # end timestamp
+    update_time = str(end_time - start_time)
+
+    td = pd.DataFrame({"update_time": [update_time]}, columns=["update_time"])
+    td.to_sql("update_time", conn, if_exists="replace")  # write data to database
+
+    conn.close()  # close the connection
