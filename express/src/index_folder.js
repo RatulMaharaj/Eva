f77a03feb9d1d535fec05646c0e4b5792c1475c5
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-22 23:23:19 +0200

    added update functionality

---
+++ express/src/index_folder.js
@@ -0,0 +1,51 @@
+const { fdir } = require("fdir");
+const fs = require('fs');
+const path = require("path");
+
+module.exports.get_stats = function get_stats(folders) {
+    var paths = []
+
+    folders.forEach(folder => {
+        // get file paths
+        const results_path = new fdir()
+            .withFullPaths()
+            .crawl(folder)
+            .sync() // get files synchronously
+        paths = paths.concat(results_path)
+    });
+
+    // get file names
+    var id = []
+    var names = []
+    var is_folder = []
+    var size_bytes = []
+    var accessed_time = []
+    var modified_time = []
+    var created_time = []
+
+    paths.forEach(item => {
+        id.push(paths.indexOf(item))
+        names.push(path.basename(item))
+        try {
+            const stats = fs.statSync(item)
+            is_folder.push(stats.isDirectory())
+            size_bytes.push(stats.size)
+            accessed_time.push(stats.atime)
+            modified_time.push(stats.mtime)
+            created_time.push(stats.ctime)
+        } catch (err) {
+            console.error(err)
+        }
+    });
+
+    return ({
+        'id': id,
+        'name': names,
+        'path': paths,
+        'is_folder': is_folder,
+        'size_bytes': size_bytes,
+        'accessed_time': accessed_time,
+        'modified_time': modified_time,
+        'created_time': created_time
+    })
+}
---
+++ express/src/update.js
@@ -0,0 +1,62 @@
+const sqlite3 = require('sqlite3').verbose();
+const LineReaderSync = require("line-reader-sync")
+var index_folder = require('./index_folder.js')
+
+function get_folders(folder_location) {
+    // array of folders to index
+    const folders = new LineReaderSync(folder_location).toLines()
+    return folders
+}
+
+function update(database_location, folder_location) {
+    
+    // data we want to store
+    const data = index_folder.get_stats(get_folders(folder_location))
+    
+    // connect to the database
+    let db = new sqlite3.Database(database_location, sqlite3.OPEN_READWRITE)
+    db.serialize(() => {
+
+        db.run(`DROP TABLE IF EXISTS ask_eva`);
+
+        db.run(`CREATE TABLE IF NOT EXISTS ask_eva(id integer, 
+            name text, 
+            path text,
+            is_folder text,
+            size_bytes numeric,
+            accessed_time text,
+            modified_time text,
+            created_time text)`)
+
+        for (const i in data.id) {
+            var row = [
+                data.id[i],
+                data.name[i],
+                data.path[i],
+                data.is_folder[i],
+                data.size_bytes[i],
+                data.accessed_time[i],
+                data.modified_time[i],
+                data.created_time[i]
+            ]
+            let sql = `INSERT INTO ask_eva (
+                id, 
+                name,
+                path, 
+                is_folder, 
+                size_bytes, 
+                accessed_time, 
+                modified_time, 
+                created_time) 
+                VALUES ((?), (?), (?), (?), (?), (?), (?), (?)) `;
+            db.run(sql, row, function (err) {
+                if (err) {
+                    return console.log(err.message);
+                }
+            })
+
+        }
+    })
+}
+
+update('../../dependencies/test.db', '../../dependencies/folders.txt')
