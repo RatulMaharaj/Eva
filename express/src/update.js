49cde581f8b00c9ada27f61a81551210fcef2121
Ratul Maharaj <ratulmaharaj@gmail.com>
2020-09-22 23:40:29 +0200

    added function to write folders to txt file

--- express/src/update.js
+++ express/src/update.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const sqlite3 = require('sqlite3').verbose();
 const LineReaderSync = require("line-reader-sync")
 var index_folder = require('./index_folder.js')
@@ -8,11 +9,15 @@ function get_folders(folder_location) {
     return folders
 }
 
+function set_folders(folder_location, lines) {
+    fs.writeFile(folder_location, lines.join('\n'), 'ascii', () => {});
+}
+
 function update(database_location, folder_location) {
-    
+
     // data we want to store
     const data = index_folder.get_stats(get_folders(folder_location))
-    
+
     // connect to the database
     let db = new sqlite3.Database(database_location, sqlite3.OPEN_READWRITE)
     db.serialize(() => {
@@ -40,15 +45,15 @@ function update(database_location, folder_location) {
                 data.created_time[i]
             ]
             let sql = `INSERT INTO ask_eva (
-                id, 
-                name,
-                path, 
-                is_folder, 
-                size_bytes, 
-                accessed_time, 
-                modified_time, 
-                created_time) 
-                VALUES ((?), (?), (?), (?), (?), (?), (?), (?)) `;
+                    id, 
+                    name,
+                    path, 
+                    is_folder, 
+                    size_bytes, 
+                    accessed_time, 
+                    modified_time, 
+                    created_time) 
+                    VALUES ((?), (?), (?), (?), (?), (?), (?), (?)) `;
             db.run(sql, row, function (err) {
                 if (err) {
                     return console.log(err.message);
@@ -58,5 +63,3 @@ function update(database_location, folder_location) {
         }
     })
 }
-
-update('../../dependencies/test.db', '../../dependencies/folders.txt')
